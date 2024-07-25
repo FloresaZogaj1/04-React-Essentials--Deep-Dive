d5ea7d7a4cb700091af7fbb5d61ae8d2e869df0a
Floresa Zogaj <floresaa.zogaj@gmail.com>
2024-07-25 17:10:37 +0200

    React Essentials - Deep Dive

--- src/data.js
+++ src/data.js
@@ -56,7 +56,8 @@ function Welcome() {
       'Components accept arbitrary inputs called props. They are like function arguments.',
     code: `
 function Welcome(props) {
-  return <h1>Hello, {props.name}</h1>;
+  return <h1>Hello, {props.name}
+  </h1>;
 }`,
   },
   state: {
@@ -65,7 +66,8 @@ function Welcome(props) {
       'State allows React components to change their output over time in response to user actions, network responses, and anything else.',
     code: `
 function Counter() {
-  const [isVisible, setIsVisible] = useState(false);
+  const [isVisible, setIsVisible]
+   = useState(false);
 
   function handleClick() {
     setIsVisible(true);
@@ -73,8 +75,10 @@ function Counter() {
 
   return (
     <div>
-      <button onClick={handleClick}>Show Details</button>
-      {isVisible && <p>Amazing details!</p>}
+      <button onClick={handleClick}>
+      Show Details</button>
+      {isVisible &&
+       <p>Amazing details!</p>}
     </div>
   );
 }`,
