2aae85d2af57fb4004376487a935a5dc9a8e595b
Floresa Zogaj <floresaa.zogaj@gmail.com>
2024-07-25 17:16:46 +0200

    React Essentials - Deep Dive

--- src/data.js
+++ src/data.js
@@ -75,7 +75,8 @@ function Counter() {
 
   return (
     <div>
-      <button onClick={handleClick}>
+      <button onClick={handleClick}
+      >
       Show Details</button>
       {isVisible &&
        <p>Amazing details!</p>}
